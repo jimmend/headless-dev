52da2a62c28824c64e16f176c99f32c11de4534e
Jim Mendes <jimrmendes@gmail.com>
2019-10-23 23:49:47 -0700

    dev

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,32 +14,31 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
-
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
+          href={`${withPrefix('/')}img/jm-logo-180px.png`}
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/jimmendes-logo.svg`}
+          href={`${withPrefix('/')}img/jm-logo-32px.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/jimmendes-logo.svg`}
+          href={`${withPrefix('/')}img/jm-logo-16px.png`}
           sizes="16x16"
         />
 
         <link
           rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
+          href={`${withPrefix('/')}img/jm-logo-180px.png`}
           color="#ff4400"
         />
+        <script src="https://kit.fontawesome.com/878a49801e.js" crossorigin="anonymous"></script>
         <meta name="theme-color" content="#fff" />
-
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import logo from '../img/jimmendes-logo.svg'
+import logo from '../img/jimmendes-logo-32px.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -41,8 +41,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item logo" title="Logo">
-              <img src={logo} alt="jimmendes.com logo" style={{ maxHeight: '30px', marginRight: '10px' }} />
-              <span className="logo-text">jimmendes.com</span>
+              <img src={logo} alt="jimmendes.com logo" style={{ height: '20px', marginRight: '10px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
