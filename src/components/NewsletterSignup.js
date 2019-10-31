16ad3495a9b2b95c5af3e5b3c04c3288a1f6dcfb
Jim Mendes <jimrmendes@gmail.com>
2019-10-31 13:25:04 -0700

    refactor newsletter sign-up form

--- src/components/NewsletterSignup.js
+++ src/components/NewsletterSignup.js
@@ -1,79 +1,121 @@
 import React from "react";
+import { navigate } from 'gatsby-link'
 
-const encode = (data) => {
-  const formData = new FormData()
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
-  for (const key of Object.keys(data)) {
-    formData.append(key, data[key])
+export default class NewsletterSignup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isValidated: false };
   }
 
-  return formData
-}
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-const handleSubmit = (e) => {
-  e.preventDefault()
-  const form = e.target
-  fetch('/', {
-    method: 'POST',
-    body: encode({
-      'form-name': form.getAttribute('name'),
-      
-    }),
-  })
-    .then(() => navigate(form.getAttribute('action')))
-    .catch(error => alert(error))  
-}
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
 
-const NewsletterSignup = () => (
-  <section className="section section-dark newsletter-signup">
-    <div className="container">
-      <p className="title">Sign Up for the Newsletter</p>
-      <div className="columns">
-        <div className="column is-one-quarter-tablet padding-column"></div>
-        <div className="column">
-          <p>Would you like to be notified when new stuff gets published?</p>
-          <p>Enter your information below.</p>
-          <form 
-            name="newsletter-signup" 
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            action="/contact/thanks/"
-            onSubmit={handleSubmit} 
-            method="post">
-            <div className="field">
-              <div className="control">
-                <input className="input" type="text" placeholder="First Name" name="firstName"/>
-              </div>
+  render() {
+    return (
+      <section className="section section-dark newsletter-signup">
+        <div className="container">
+          <p className="title">Sign Up for the Newsletter</p>
+          <div className="columns">
+            <div className="column is-one-quarter-tablet padding-column"></div>
+            <div className="column">
+              <p>
+                Would you like to be notified when new stuff gets published?
+              </p>
+              <p>Enter your information below.</p>
+              <form
+                name="newsletter-signup"
+                method="post"
+                action="/contact/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+              >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <div hidden>
+                  <label>
+                    Don’t fill this out:{" "}
+                    <input name="bot-field" onChange={this.handleChange} />
+                  </label>
+                </div>
+                <div className="field">
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={'text'}
+                      name={'firstName'}
+                      onChange={this.handleChange}
+                      id={'firstName'}
+                      required={true}
+                      placeholder="First Name"
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="control">
+                  <input
+                      className="input"
+                      type={'text'}
+                      name={'lastName'}
+                      onChange={this.handleChange}
+                      id={'lastName'}
+                      required={true}
+                      placeholder="Last Name"
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="control">
+                  <input
+                      className="input"
+                      type={'email'}
+                      name={'email'}
+                      onChange={this.handleChange}
+                      id={'email'}
+                      required={true}
+                      placeholder="EMail"
+                    />
+                  </div>
+                </div>
+                <div className="field is-clearfix">
+                  <button
+                    type="submit"
+                    className="button is-link is-red is-pulled-right"
+                  >
+                    SUBMIT
+                  </button>
+                </div>
+              </form>
+              <p className="no-bs">
+                Unsubscribe any time. No junk, spam or BS.
+              </p>
             </div>
-            <div className="field">
-              <div className="control">
-                <input className="input" type="text" placeholder="Last Name" name="lastName" />
-              </div>
-            </div>
-            <div className="field">
-              <div className="control">
-                <input
-                  className="input"
-                  type="email"
-                  placeholder="Email Address"
-                  name="email"
-                />
-              </div>
-            </div>
-            <div className="field is-clearfix">
-              <button type="submit" className="button is-link is-red is-pulled-right">
-                SUBMIT
-              </button>
-            </div>
-          </form>
-          <p className="no-bs">
-            Unsubscribe any time. No junk, spam or BS.
-          </p>
+            <div className="column is-one-quarter-tablet padding-column"></div>
+          </div>
         </div>
-        <div className="column is-one-quarter-tablet padding-column"></div>
-      </div>
-    </div>
-  </section>
-);
-
-export default NewsletterSignup;
+      </section>
+    )
+  }
+}
